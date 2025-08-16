c21c4092ba98e7ac8dd5f085d06800a595d39d34
NikitaYatsko <archon04@mail.ru>
2025-08-16 12:53:59 +0300

    handle Exceptions

--- src/main/java/com/post_hub/iam_service/controller/PostController.java
+++ src/main/java/com/post_hub/iam_service/controller/PostController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("${end.point.posts}")
 public class PostController {
-    private final PostRepository postRepository;
+
     private final PostService postService;
 
     @GetMapping("${end.point.id}")
