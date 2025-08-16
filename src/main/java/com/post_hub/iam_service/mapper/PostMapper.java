e4c0387ca8b15b1304e12c5c7ad60f90eb67bc8b
NikitaYatsko <archon04@mail.ru>
2025-08-16 13:21:15 +0300

    mapper

---
+++ src/main/java/com/post_hub/iam_service/mapper/PostMapper.java
@@ -0,0 +1,22 @@
+package com.post_hub.iam_service.mapper;
+import com.post_hub.iam_service.model.dto.post.PostDTO;
+import com.post_hub.iam_service.model.enteties.Post;
+import org.hibernate.type.descriptor.DateTimeUtils;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.springframework.context.annotation.Bean;
+
+@Mapper(componentModel = "spring",
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        imports = {DateTimeUtils.class, Object.class}
+)
+
+public interface PostMapper {
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "title", target = "title")
+    @Mapping(source = "content",target = "content")
+    @Mapping(source = "likes",target = "likes")
+    @Mapping(source = "created",target = "created", dateFormat = "yyyy-MM-dd'T'HH:mm:ss")
+    PostDTO toPostDTO(Post post);
+}
--- src/main/java/com/post_hub/iam_service/service/Impl/PostServiceImpl.java
+++ src/main/java/com/post_hub/iam_service/service/Impl/PostServiceImpl.java
@@ -1,5 +1,6 @@
 package com.post_hub.iam_service.service.Impl;
 
+import com.post_hub.iam_service.mapper.PostMapper;
 import com.post_hub.iam_service.model.constants.ApiErrorMessage;
 import com.post_hub.iam_service.model.dto.post.PostDTO;
 import com.post_hub.iam_service.model.enteties.Post;
@@ -16,21 +17,14 @@ import org.springframework.stereotype.Service;
 public class PostServiceImpl implements PostService {
 
     private final PostRepository postRepository;
+    private final PostMapper postMapper;
 
 
     @Override
     public IamResponse<PostDTO> getById(@NotNull Integer postId) {
         Post post = postRepository.findById(postId).orElseThrow(
                 () -> new NotFoundException(ApiErrorMessage.POST_NOT_FOUND_BY_ID.getMessage(postId)));
-
-        PostDTO postDTO = PostDTO.builder()
-                .id(post.getId())
-                .title(post.getTitle())
-                .content(post.getContent())
-                .likes(post.getLikes())
-                .created(post.getCreated())
-                .build();
-
+        PostDTO postDTO = postMapper.toPostDTO(post);
         return IamResponse.сreateSuccessful(postDTO);
 
 
